b65792e48c9f548968e9ffa11822a8b4c3755df8
waseemyusuf <whossain@ualberta.ca>
2022-01-14 15:36:14 -0700

    Print its own source

--- script.py
+++ script.py
@@ -6,3 +6,6 @@ if __name__ == '__main__':
 
     print(requests.get('http://google.com/'))
 
+    url = 'https://raw.githubusercontent.com/waseemyusuf/cmput404_lab1/main/script.py'
+    print(requests.get(url).text)
+
